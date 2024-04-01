6c9154e9ad0446e376a3cad024e6f05346b870a1
Sh031224 <1cktmdgh2@gmail.com>
2024-04-01 14:30:20 +0900

    docs: navigation 가이드 추가

--- docs/src/docs/routes.ts
+++ docs/src/docs/routes.ts
@@ -58,6 +58,7 @@ export const routes: Array<Route> = [
       { title: 'Container', slug: '/docs/utility/container' },
       { title: 'Gradient', slug: '/docs/utility/gradient' },
       { title: 'Media', slug: '/docs/utility/media' },
+      { title: 'Navigation', slug: '/docs/utility/navigation' },
       { title: 'Opacity', slug: '/docs/utility/opacity' },
       { title: 'Typography', slug: '/docs/utility/typography' },
     ],
