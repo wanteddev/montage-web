eeef23bd64bd5ca7826b8742e9f8250921b91295
Sh031224 <1cktmdgh2@gmail.com>
2024-04-04 11:03:58 +0900

    feat(wds): typescript 이전 버전 호환성 추가

--- packages/wds/declaration.d.ts
+++ packages/wds/declaration.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="@emotion/react/types/css-prop" />
-import { lightTheme, darkTheme } from '@wanteddev/wds';
+import { lightTheme } from '@wanteddev/wds';
 
 import '@emotion/react';
 
-type BaseTheme = typeof lightTheme | typeof darkTheme;
+type BaseTheme = typeof lightTheme;
 
 declare module '@emotion/react' {
-  export type Theme = BaseTheme;
+  export interface Theme extends BaseTheme {}
 }
