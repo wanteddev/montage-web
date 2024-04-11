7e598c75c2964d074c4b90fe2d0442b5517c0e44
Sh031224 <1cktmdgh2@gmail.com>
2024-04-11 17:08:18 +0900

    docs: code font family 변경

--- docs/src/app/providers.tsx
+++ docs/src/app/providers.tsx
@@ -2,13 +2,25 @@
 
 import { AppRouterCacheProvider } from '@wanteddev/wds-nextjs';
 import { ThemeProvider } from '@wanteddev/wds';
+import { Global } from '@emotion/react';
 
 import type { PropsWithChildren } from 'react';
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
     <AppRouterCacheProvider>
-      <ThemeProvider enableDarkMode>{children}</ThemeProvider>
+      <ThemeProvider enableDarkMode>
+        {children}
+
+        <Global
+          styles={{
+            ['code, .npm__react-simple-code-editor__textarea, pre']: {
+              fontFamily:
+                'ui-monospace,SFMono-Regular,"SF Mono",Menlo,Consolas,"Liberation Mono",monospace !important',
+            },
+          }}
+        />
+      </ThemeProvider>
     </AppRouterCacheProvider>
   );
 };
