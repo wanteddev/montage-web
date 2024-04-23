e6ff50a31d0376566f0751f77ef1b9df08bdb291
Sh031224 <1cktmdgh2@gmail.com>
2024-04-23 18:37:11 +0900

    fix(wds): modal fixed에서 이유없이 스크롤이 생기는 이슈

--- packages/wds/src/components/modal/style.ts
+++ packages/wds/src/components/modal/style.ts
@@ -353,7 +353,6 @@ export const modalContentStyle =
   }: ModalContentProps) =>
   (theme: Theme) => css`
     width: 100%;
-    height: 100%;
     padding-bottom: var(--wds-modal-content-margin, 20px);
 
     ${modalContentPadding({
