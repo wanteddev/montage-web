4a98dcf8f4558f57c4a34638dd5fe42129f97017
Sh031224 <1cktmdgh2@gmail.com>
2024-04-23 17:25:11 +0900

    fix(wds): theme provider 로컬 스토리지에 이상한 값이 있는 경우 오류 발생

--- packages/wds/src/hooks/use-theme-control.ts
+++ packages/wds/src/hooks/use-theme-control.ts
@@ -1,11 +1,24 @@
-import { useThemeStore } from '../stores/theme-store';
+import { useMemo } from 'react';
+import { useTheme } from 'next-themes';
+
+const themes = ['light', 'dark'] as const;
 
 const useThemeControl = () => {
-  const { resolvedTheme, setTheme } = useThemeStore((state) => state);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const theme = useMemo(() => {
+    if (!resolvedTheme) {
+      return 'light';
+    }
+
+    return themes.includes(resolvedTheme as any)
+      ? (resolvedTheme as (typeof themes)[number])
+      : 'light';
+  }, [resolvedTheme]);
 
   return {
-    theme: resolvedTheme ? (resolvedTheme as 'light' | 'dark') : 'light',
-    setTheme: setTheme as (theme: 'light' | 'dark' | 'system') => void,
+    theme,
+    setTheme,
   };
 };
 
--- packages/wds/src/theme-provider/index.tsx
+++ packages/wds/src/theme-provider/index.tsx
@@ -1,35 +1,43 @@
 'use client';
 import { ThemeProvider as DefaultThemeProvider, Global } from '@emotion/react';
+import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import { type PropsWithChildren, useMemo } from 'react';
 
 import { theme as themes } from '../theme';
 import useThemeControl from '../hooks/use-theme-control';
 
-import NextThemeProvider from './next-theme-provider';
 import StoreProvider from './store-provider';
 
-import type { ThemeProviderProps } from './next-theme-provider';
 import type { Theme } from '@emotion/react';
 
-type Props = ThemeProviderProps & {
+type Props = PropsWithChildren<{
+  enableDarkMode?: boolean;
+  /** Disable all CSS transitions when switching themes */
+  disableTransitionOnChange?: boolean | undefined;
+  /** Key used to store theme setting in localStorage */
+  storageKey?: string | undefined;
+  /* 강제로 테마를 덮어씌울 때 사용합니다. */
+  forcedTheme?: 'light' | 'dark' | undefined;
   /** Use default global style */
   disableDefaultGlobalStyle?: boolean | undefined;
-};
+}>;
 
 const ThemeProvider = ({
   children,
-  enableDarkMode = false,
-  defaultTheme = enableDarkMode ? 'system' : 'light',
+  enableDarkMode,
   disableTransitionOnChange = false,
+  forcedTheme,
   storageKey = 'theme',
   disableDefaultGlobalStyle = false,
 }: Props) => {
   return (
     <NextThemeProvider
-      enableDarkMode={enableDarkMode}
+      themes={enableDarkMode ? ['light', 'dark'] : ['light']}
+      enableSystem={enableDarkMode || false}
+      enableColorScheme
       disableTransitionOnChange={disableTransitionOnChange}
+      forcedTheme={forcedTheme}
       storageKey={storageKey}
-      defaultTheme={defaultTheme}
     >
       <EmotionThemeProvider
         disableDefaultGlobalStyle={disableDefaultGlobalStyle}
