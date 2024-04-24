841c277238e27e5153a14453e0ed238ac03c39cf
Sh031224 <1cktmdgh2@gmail.com>
2024-04-24 15:17:34 +0900

    feat(wds-nextjs): page router 구성 추가

--- packages/wds-nextjs/src/index.ts
+++ packages/wds-nextjs/src/index.ts
@@ -1,1 +1,2 @@
 export { default as AppRouterCacheProvider } from './app-router-cache-provider';
+export * from './pages-router-cache-provider';
---
+++ packages/wds-nextjs/src/pages-router-cache-provider.tsx
@@ -0,0 +1,120 @@
+import * as React from 'react';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import createEmotionServer from '@emotion/server/create-instance';
+
+import type Document from 'next/document';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import type { AppType } from 'next/app';
+import type { EmotionCache } from '@emotion/react';
+
+const createEmotionCache = () => {
+  let insertionPoint;
+
+  if (typeof document !== 'undefined') {
+    const emotionInsertionPoint = document.querySelector<HTMLMetaElement>(
+      'meta[name="emotion-insertion-point"]',
+    );
+    insertionPoint = emotionInsertionPoint ?? undefined;
+  }
+
+  return createCache({ key: 'wds', insertionPoint });
+};
+
+export type EmotionCacheProviderProps = {
+  emotionCache?: EmotionCache;
+};
+
+const defaultEmotionCache = createEmotionCache();
+
+export const AppCacheProvider = ({
+  emotionCache = defaultEmotionCache,
+  children,
+}: React.PropsWithChildren<EmotionCacheProviderProps>) => {
+  return <CacheProvider value={emotionCache}>{children}</CacheProvider>;
+};
+
+// //////////////////////////////////////////////////
+// _document
+
+export type DocumentHeadTagsProps = {
+  emotionStyleTags: Array<React.ReactElement>;
+};
+export const DocumentHeadTags = (props: DocumentHeadTagsProps) => {
+  return (
+    <>
+      <meta name="emotion-insertion-point" content="" />
+      {props.emotionStyleTags}
+    </>
+  );
+};
+
+export type Enhancer<C> = (Component: C) => C;
+
+type Plugin = {
+  enhanceApp: Enhancer<AppType>;
+  resolveProps: (
+    initialProps: DocumentInitialProps,
+  ) => Promise<DocumentInitialProps>;
+};
+
+const createGetInitialProps = (
+  document: typeof Document,
+  plugins: Array<Plugin>,
+) => {
+  return async function getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) =>
+          plugins.reduce((result, plugin) => plugin.enhanceApp(result), App),
+      });
+
+    const initialProps = await document.getInitialProps(ctx);
+
+    const finalProps = await plugins.reduce(
+      async (result, plugin) => plugin.resolveProps(await result),
+      Promise.resolve(initialProps),
+    );
+
+    return finalProps;
+  };
+};
+
+export async function documentGetInitialProps(
+  document: typeof Document,
+  ctx: DocumentContext,
+  options?: {
+    emotionCache?: EmotionCache;
+    plugins?: Array<Plugin>;
+  },
+) {
+  const cache = options?.emotionCache ?? createEmotionCache();
+
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  return createGetInitialProps(document, [
+    {
+      enhanceApp: (App: any) =>
+        function EnhanceApp(props) {
+          return <App emotionCache={cache} {...props} />;
+        },
+      resolveProps: async (initialProps) => {
+        const { styles } = extractCriticalToChunks(initialProps.html);
+        return {
+          ...initialProps,
+          emotionStyleTags: styles.map((style) => (
+            <style
+              data-emotion={`${style.key} ${style.ids.join(' ')}`}
+              key={style.key}
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{ __html: style.css }}
+            />
+          )),
+        };
+      },
+    },
+    ...(options?.plugins ?? []),
+  ])(ctx) as Promise<DocumentInitialProps & DocumentHeadTagsProps>;
+}
