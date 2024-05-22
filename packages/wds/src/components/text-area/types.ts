bbf2b1a3793496ee660c2d68109018ccc492007b
차승호 <1cktmdgh2@gmail.com>
2024-05-22 14:39:31 +0900

    fix(wds): text area의 disabled가 동작하지 않음 (#11)

--- packages/wds/src/components/text-area/types.ts
+++ packages/wds/src/components/text-area/types.ts
@@ -3,7 +3,15 @@ import type { CSSProperties, ReactNode } from 'react';
 
 export type TextAreaDefaultProps = {
   invalid?: boolean;
+  /**
+   * 실제 dom에는 전달 되지 않으며 아래 글자 수를 표시할 때 사용합니다.
+   * react-hook-form의 rules와 함께 사용해야합니다.
+   */
   maxLength?: number;
+  /**
+   * 실제 dom에 전달 되는 maxLength 옵션입니다.
+   */
+  htmlMaxLength?: number;
   rightIcon?: ReactNode;
   disabled?: boolean;
   width?: CSSProperties['width'];
