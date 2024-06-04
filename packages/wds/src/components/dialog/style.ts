2ba8fd42a05d3530cab6001028250cabc748645e
Sh031224 <1cktmdgh2@gmail.com>
2024-06-04 11:38:26 +0900

    fix(wds): dialog 크기가 작은 디바이스 대응

--- packages/wds/src/components/dialog/style.ts
+++ packages/wds/src/components/dialog/style.ts
@@ -1,5 +1,7 @@
 import { css } from '@wanteddev/wds-engine';
 
+import { respondTo } from '../../utils';
+
 import type { Theme } from '@wanteddev/wds-engine';
 
 export const dialogWrapperStyle = (theme: Theme) => css`
@@ -31,6 +33,7 @@ export const dialogStyle = css`
   align-items: center;
   justify-content: center;
   outline: 0;
+  flex: 1;
 `;
 
 export const dialogContentStyle = (theme: Theme) => css`
@@ -39,6 +42,10 @@ export const dialogContentStyle = (theme: Theme) => css`
   min-width: 320px;
   max-width: 100%;
   outline: none;
+
+  ${respondTo('360px')} {
+    min-width: 100%;
+  }
 `;
 
 export const dialogDividerStyle = (theme: Theme) => css`
