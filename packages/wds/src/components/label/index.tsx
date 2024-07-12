37318a0e384d58fd1bb527f2f92798cb3eba2c8d
차승호 <1cktmdgh2@gmail.com>
2024-07-12 12:55:24 +0900

    feat(wds): text-field, label 디자인 및 구조 변경 (#56)

--- packages/wds/src/components/label/index.tsx
+++ packages/wds/src/components/label/index.tsx
@@ -3,22 +3,36 @@ import { forwardRef } from 'react';
 
 import Typography from '../typography';
 
-import type { DefaultComponentProps } from '@wanteddev/wds-engine';
+import type { DefaultComponentProps, Merge } from '@wanteddev/wds-engine';
 import type { TypographyProps } from '../typography/types';
 
 const Label = forwardRef<
   HTMLLabelElement,
-  DefaultComponentProps<TypographyProps, 'label'>
->((props, ref) => {
+  Merge<{ required?: boolean }, DefaultComponentProps<TypographyProps, 'label'>>
+>(({ display = 'inline-block', required, children, ...props }, ref) => {
   return (
     <Typography
       variant="label1_normal"
-      weight="bold"
-      color="palette.label.alternative"
+      weight="medium"
+      color="palette.label.neutral"
       as="label"
       ref={ref}
+      display={display}
       {...props}
-    />
+    >
+      {children}
+      {required && (
+        <Typography
+          variant="label1_normal"
+          weight="medium"
+          display="inline-block"
+          sx={{ marginLeft: '4px' }}
+          color="palette.status.negative"
+        >
+          *
+        </Typography>
+      )}
+    </Typography>
   );
 });
 
