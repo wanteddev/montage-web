0f146ac8e2405ff45e34a47dd5a6b01e5e798ae2
차승호 <1cktmdgh2@gmail.com>
2024-08-05 10:24:23 +0900

    feat(wds): dialog 확장성 있는 인터페이스로 변경 (#72)

--- packages/wds/src/components/index.ts
+++ packages/wds/src/components/index.ts
@@ -11,6 +11,7 @@ export { default as ChipFilter } from './chip-filter';
 export { default as ChipMultiSelect } from './chip-multi-select';
 export * from './compact-tooltip';
 export { default as ContentBadge } from './content-badge';
+export { DialogButton } from './dialog';
 export { default as DismissableLayer } from './dismissable-layer';
 export { default as Divider } from './divider';
 export { default as FlexBox } from './flex-box';
