a96d33cdd7072c9b7d1499c05e15971ec59a464b
1000peach <iam1000peach@gmail.com>
2024-08-26 16:16:13 +0900

    docs: list 문서 수정

--- packages/wds/src/components/list/index.tsx
+++ packages/wds/src/components/list/index.tsx
@@ -123,8 +123,11 @@ const ListItem = forwardRef(
                     return;
                   }
 
-                  // controllable 직접 클릭 시 이벤트 중복 호출을 방어함.
-                  if (!controllable?.contains(e.target as HTMLElement)) {
+                  if (
+                    controllable &&
+                    // controllable 직접 클릭 시 이벤트 중복 호출을 방어함.
+                    !controllable.contains(e.target as HTMLElement)
+                  ) {
                     (controllable as HTMLElement).click();
                     (controllable as HTMLElement).focus();
                   }
