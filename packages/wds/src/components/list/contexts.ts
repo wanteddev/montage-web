02599c09968ea60969d6a5bf7e54b15edde49a04
차승호 <1cktmdgh2@gmail.com>
2024-08-30 11:06:13 +0900

    feat(wds): list 디자인 QA 및 인터페이스, 동작 고도화 (#81)

--- packages/wds/src/components/list/contexts.ts
+++ packages/wds/src/components/list/contexts.ts
@@ -5,7 +5,6 @@ import { LIST_ITEM_NAME } from './constants';
 type ListItemContextType = {
   active: boolean;
   disabled: boolean;
-  hasLabelTargetElement: boolean;
 };
 
 export const [ListItemProvider, useListItemContext] =
