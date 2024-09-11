2fdfa7de014354074aa3d404d50b77a1058a6063
1000peach <iam1000peach@gmail.com>
2024-09-11 13:47:58 +0900

    style(wds): select-multiple gradient 색상 변경

--- packages/wds/src/components/select-multiple/style.ts
+++ packages/wds/src/components/select-multiple/style.ts
@@ -2,24 +2,26 @@ import { css } from '@wanteddev/wds-engine';
 
 import { gradient } from '../../utils';
 
-import type { Theme } from '@wanteddev/wds-engine';
+export const customSelectMultipleRenderWrapperStyle = ({
+  overflow,
+  isScrollable,
+}: {
+  overflow: boolean;
+  isScrollable: boolean;
+}) =>
+  overflow === false &&
+  css`
+    flex-wrap: nowrap;
+    overflow-y: hidden;
 
-export const customSelectMultipleRenderWrapperStyle =
-  ({ overflow, isScrollable }: { overflow: boolean; isScrollable: boolean }) =>
-  (theme: Theme) =>
-    overflow === false &&
+    &::-webkit-scrollbar {
+      display: none;
+    }
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+
+    ${isScrollable &&
     css`
-      flex-wrap: nowrap;
-      overflow-y: hidden;
-
-      &::-webkit-scrollbar {
-        display: none;
-      }
-      -ms-overflow-style: none;
-      scrollbar-width: none;
-
-      ${isScrollable &&
-      css`
-        ${gradient(theme.palette.background.normal.normal, 'right', '40px')}
-      `}
-    `;
+      ${gradient('transparent', 'right', '40px')}
+    `}
+  `;
