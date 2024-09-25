68f022877257133613795b5c5a18b00c0293f325
Sh031224 <1cktmdgh2@gmail.com>
2024-09-25 11:16:22 +0900

    fix(wds): safari에서 1.5px box-shadow를 이상하게 렌더링 하는 이슈

--- packages/wds/src/components/checkbox/style.ts
+++ packages/wds/src/components/checkbox/style.ts
@@ -50,6 +50,25 @@ export const checkboxStyle =
       border-radius: inherit;
       box-shadow: inset 0 0 0 1.5px ${theme.palette.line.normal.normal};
 
+      // safari
+      @supports (-webkit-backdrop-filter: none) {
+        box-shadow: inset 0 0 0 1px ${theme.palette.line.normal.normal};
+
+        @media only screen and (-webkit-device-pixel-ratio: 2),
+          only screen and (-moz-device-pixel-ratio: 2),
+          only screen and (device-pixel-ratio: 2) {
+          box-shadow: inset 0 0 0 1.5px ${theme.palette.line.normal.normal};
+        }
+
+        @media only screen and (-webkit-device-pixel-ratio: 3),
+          only screen and (-moz-device-pixel-ratio: 3),
+          only screen and (device-pixel-ratio: 3) {
+          box-shadow: inset 0 0 0 1.33333333px
+            ${theme.palette.line.normal.normal};
+          transform: translate(0);
+        }
+      }
+
       transition:
         background-color 0.2s ease,
         box-shadow 0.2s ease;
