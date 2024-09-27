799e5b45ff97c67132676f368984cf2df93b8af1
Sh031224 <1cktmdgh2@gmail.com>
2024-09-27 18:24:10 +0900

    fix(wds): media 계산식에 px 추가

--- packages/wds/src/utils/media.ts
+++ packages/wds/src/utils/media.ts
@@ -6,7 +6,7 @@
  * respondTo('768px');
  */
 export const respondTo = (breakpoint: string) =>
-  `@media only screen and (max-width: ${parseInt(breakpoint, 10) - 1})`;
+  `@media only screen and (max-width: ${parseInt(breakpoint, 10) - 1}px)`;
 
 /**
  * @description `respondUp` 와 같은 역할을 하며 지정한 breakpoint 보다 작을 때 동작합니다.
@@ -16,7 +16,7 @@ export const respondTo = (breakpoint: string) =>
  * respondUp('768px');
  */
 export const respondMore = (breakpoint: string) =>
-  `@media only screen and (min-width: ${breakpoint}px)`;
+  `@media only screen and (min-width: ${breakpoint})`;
 
 /**
  * @description `respondTo` 와 같은 역할을 하며 지정한 breakpoint 보다 작을 때 동작합니다.
@@ -26,7 +26,7 @@ export const respondMore = (breakpoint: string) =>
  * respondDown('768px');
  */
 export const respondDown = (breakpoint: string) =>
-  `@media only screen and (max-width: ${parseInt(breakpoint, 10) - 1})`;
+  `@media only screen and (max-width: ${parseInt(breakpoint, 10) - 1}px)`;
 
 /**
  * @description `respondMore` 와 같은 역할을 하며 지정한 breakpoint 보다 클 때 동작합니다.
@@ -36,4 +36,4 @@ export const respondDown = (breakpoint: string) =>
  * respondDown('768px');
  */
 export const respondUp = (breakpoint: string) =>
-  `@media only screen and (min-width: ${breakpoint}px)`;
+  `@media only screen and (min-width: ${breakpoint})`;
