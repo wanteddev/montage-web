aeee93a517d2300f3fe1467c0ad4d175bf2f3253
차승호 <1cktmdgh2@gmail.com>
2024-10-08 17:43:20 +0900

    feat(wds): PI-68532 slider 컴포넌트 추가 (#126)

---
+++ packages/wds/src/components/slider/style.ts
@@ -0,0 +1,108 @@
+import { css } from '@wanteddev/wds-engine';
+
+import type { SliderProps } from './types';
+import type { Theme } from '@wanteddev/wds-engine';
+
+export const sliderProgressWrapperStyle =
+  ({ disabled }: SliderProps) =>
+  (theme: Theme) => css`
+    padding: 8px;
+    border-radius: 1000px;
+    position: relative;
+
+    ${disabled
+      ? css`
+          cursor: initial;
+
+          [data-role='slider-progress-range'] {
+            background-color: ${theme.palette.interaction.disable};
+          }
+
+          [data-role='slider-progress'] {
+            background-color: ${theme.palette.interaction.disable};
+          }
+        `
+      : css`
+          cursor: pointer;
+
+          [data-role='slider-progress-range'] {
+            background-color: ${theme.palette.fill.strong};
+          }
+
+          [data-role='slider-progress'] {
+            background-color: ${theme.palette.primary.normal};
+          }
+        `}
+  `;
+
+export const sliderProgressStyle = css`
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+  height: 4px;
+  border-radius: inherit;
+`;
+
+export const sliderProgressRangeStyle = css`
+  position: absolute;
+  border-radius: inherit;
+  height: 100%;
+`;
+
+export const sliderThumbStyle = (theme: Theme) => css`
+  width: 20px;
+  height: 20px;
+  border-radius: 9999px;
+  position: absolute;
+  top: 0px;
+  display: block;
+  cursor: pointer;
+
+  &[aria-disabled='true'] {
+    pointer-events: none;
+    cursor: initial;
+
+    [data-role='slider-thumb-knob'] {
+      background-color: ${theme.palette.interaction.disable};
+    }
+  }
+
+  &:hover [data-role='slider-thumb-interaction'] {
+    opacity: 0.075;
+  }
+  &:focus,
+  &:focus-visible {
+    outline: none;
+    [data-role='slider-thumb-interaction'] {
+      opacity: 0.075;
+    }
+  }
+
+  &:active [data-role='slider-thumb-interaction'] {
+    opacity: 0.075;
+  }
+`;
+
+export const sliderThumbInteractionStyle = (theme: Theme) => css`
+  background-color: ${theme.palette.primary.normal};
+  opacity: 0;
+  width: calc(100% + 12px);
+  height: calc(100% + 12px);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border-radius: inherit;
+  display: inline-block;
+  transition: opacity 0.15s ease;
+`;
+
+export const sliderThumbKnobStyle = (theme: Theme) => css`
+  background-color: ${theme.palette.primary.normal};
+  border: 2px solid ${theme.palette.background.normal.normal};
+  border-radius: inherit;
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  position: relative;
+`;
