baea5daa01541067c0f7f4855f088a48c0ceed31
1000peach <iam1000peach@gmail.com>
2024-11-22 15:24:05 +0900

    feat(wds): card-thumbnail-skeleton ratio custom 추가

--- packages/wds/src/components/card/index.tsx
+++ packages/wds/src/components/card/index.tsx
@@ -32,6 +32,7 @@ import {
   cardThumbnailContentTextStyle,
   cardThumbnailContentToggleIconStyle,
   cardThumbnailContentWrapperStyle,
+  cardThumbnailSkeletonStyle,
   cardThumbnailStyle,
 } from './style';
 
@@ -284,10 +285,26 @@ CardSkeleton.displayName = CARD_SKELETON_NAME;
 
 const CardThumbnailSkeleton = forwardRef(
   (
-    props: DefaultComponentProps<ThumbnailSkeletonProps, 'div'>,
+    {
+      ratio,
+      xl,
+      lg,
+      md,
+      sm,
+      xs,
+      sx,
+      ...props
+    }: DefaultComponentProps<ThumbnailSkeletonProps, 'div'>,
     ref: ForwardedRef<ElementRef<'div'>>,
   ) => {
-    return <ThumbnailSkeleton ref={ref} radius {...props} />;
+    return (
+      <ThumbnailSkeleton
+        ref={ref}
+        radius
+        sx={[cardThumbnailSkeletonStyle({ ratio, xs, sm, md, lg, xl }), sx]}
+        {...props}
+      />
+    );
   },
 );
 
