dd7506c562043fc4e0342e79409e6096ce23cc7f
1000peach <iam1000peach@gmail.com>
2024-12-10 14:55:47 +0900

    docs: root-page empty-state 컴포넌트 활용

--- docs/src/app/page.tsx
+++ docs/src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '@wanteddev/wds';
+import { EmptyState, EmptyStateButton } from '@wanteddev/wds';
 import Link from 'next/link';
 
 import ClientFullPageLayout from './full-page-layout.client';
@@ -8,14 +8,11 @@ import ClientFullPageLayout from './full-page-layout.client';
 const RootPage = () => {
   return (
     <ClientFullPageLayout>
-      <Button
-        variant="solid"
-        size="medium"
-        as={Link}
-        href="/docs/overview/getting-started"
-      >
-        문서 확인하기
-      </Button>
+      <EmptyState platform="mobile" sm={{ platform: 'desktop' }}>
+        <EmptyStateButton as={Link} href="/docs/overview/getting-started">
+          문서 확인하기
+        </EmptyStateButton>
+      </EmptyState>
     </ClientFullPageLayout>
   );
 };
