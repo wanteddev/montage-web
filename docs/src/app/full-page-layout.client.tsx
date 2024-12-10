561a88da5f0444b4169f98f1a7ef3d943bc258d3
1000peach <codingandpeach@gmail.com>
2024-12-10 19:28:29 +0900

    docs: not-found 페이지 작성 (#188)

---
+++ docs/src/app/full-page-layout.client.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { FlexBox } from '@wanteddev/wds';
+
+import type { PropsWithChildren } from 'react';
+
+const ClientFullPageLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <FlexBox
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      gap="8px"
+      sx={{ width: '100%', height: 'calc(100dvh - var(--header-height))' }}
+      sm={{
+        sx: { padding: '0px 0px 20px 20px', width: 'calc(100% - 250px)' },
+      }}
+      md={{
+        sx: {
+          padding: '0px 20px 20px 20px',
+          width: 'calc(100% - 250px - 150px)',
+        },
+      }}
+    >
+      {children}
+    </FlexBox>
+  );
+};
+
+export default ClientFullPageLayout;
---
+++ docs/src/app/not-found.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import {
+  EmptyState,
+  EmptyStateButton,
+  EmptyStateContent,
+  EmptyStateImage,
+  EmptyStateText,
+  ImageLoader,
+} from '@wanteddev/wds';
+import Link from 'next/link';
+
+import ClientFullPageLayout from './full-page-layout.client';
+
+const NotFoundPage = () => {
+  return (
+    <ClientFullPageLayout>
+      <EmptyState platform="mobile" sm={{ platform: 'desktop' }}>
+        <EmptyStateImage>
+          <ImageLoader
+            src="https://static.wanted.co.kr/images/ghost.png"
+            width={200}
+            quality={100}
+            alt="ghost"
+          />
+        </EmptyStateImage>
+        <EmptyStateContent>
+          <EmptyStateText
+            heading="페이지를 찾을 수 없어요."
+            description={
+              <>
+                잘못된 경로로 접근했거나 페이지가 삭제되었어요.
+                <br />
+                다시 시도해 주세요.
+              </>
+            }
+          />
+          <EmptyStateButton as={Link} href="/docs/overview/getting-started">
+            문서 확인하기
+          </EmptyStateButton>
+        </EmptyStateContent>
+      </EmptyState>
+    </ClientFullPageLayout>
+  );
+};
+
+export default NotFoundPage;
