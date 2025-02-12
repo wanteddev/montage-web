33b2d226f6c01060695636d9b13987574f87aedb
Sh031224 <1cktmdgh2@gmail.com>
2025-02-12 18:05:34 +0900

    feat(wds): typography variant명 변경

--- packages/wds/src/components/typography/index.tsx
+++ packages/wds/src/components/typography/index.tsx
@@ -22,7 +22,7 @@ const Typography = forwardRef(
   <E extends ElementType = 'span'>(
     {
       as,
-      variant = 'body1_normal',
+      variant = 'body1',
       weight = 'regular',
       noWrap = false,
       display = 'inline',
