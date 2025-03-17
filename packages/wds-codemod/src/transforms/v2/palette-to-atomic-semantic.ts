9740b60e73753838887a0d4e590cf164ecc0b907
Sh031224 <1cktmdgh2@gmail.com>
2025-03-17 19:11:02 +0900

    fix(wds-codemod): 누락된 마이그레이션 수정

--- packages/wds-codemod/src/transforms/v2/palette-to-atomic-semantic.ts
+++ packages/wds-codemod/src/transforms/v2/palette-to-atomic-semantic.ts
@@ -360,13 +360,13 @@ const transformer = (file: FileInfo, api: API, options: Options) => {
         name: { name: getLocalName(autocompleteOptionImport) },
       })
       .forEach((comp) => {
-        comp.value.attributes?.forEach((attr) =>
-          deepConvertPropertyValue(
-            attr as JSXAttribute,
-            'color',
-            convertColorPalette(),
-          ),
-        );
+        const textProps = (
+          comp.value.attributes?.find(
+            (v) => v.type === 'JSXAttribute' && v.name.name === 'textProps',
+          ) as JSXAttribute | undefined
+        )?.value as JSXExpressionContainer;
+
+        deepConvertPropertyValue(textProps, 'color', convertColorPalette());
       });
   }
 
@@ -383,13 +383,13 @@ const transformer = (file: FileInfo, api: API, options: Options) => {
         name: { name: getLocalName(menuItemImport) },
       })
       .forEach((comp) => {
-        comp.value.attributes?.forEach((attr) =>
-          deepConvertPropertyValue(
-            attr as JSXAttribute,
-            'color',
-            convertColorPalette(),
-          ),
-        );
+        const textProps = (
+          comp.value.attributes?.find(
+            (v) => v.type === 'JSXAttribute' && v.name.name === 'textProps',
+          ) as JSXAttribute | undefined
+        )?.value as JSXExpressionContainer;
+
+        deepConvertPropertyValue(textProps, 'color', convertColorPalette());
       });
   }
 
@@ -406,13 +406,13 @@ const transformer = (file: FileInfo, api: API, options: Options) => {
         name: { name: getLocalName(optionImport) },
       })
       .forEach((comp) => {
-        comp.value.attributes?.forEach((attr) =>
-          deepConvertPropertyValue(
-            attr as JSXAttribute,
-            'color',
-            convertColorPalette(),
-          ),
-        );
+        const textProps = (
+          comp.value.attributes?.find(
+            (v) => v.type === 'JSXAttribute' && v.name.name === 'textProps',
+          ) as JSXAttribute | undefined
+        )?.value as JSXExpressionContainer;
+
+        deepConvertPropertyValue(textProps, 'color', convertColorPalette());
       });
   }
 
@@ -781,6 +781,29 @@ const transformer = (file: FileInfo, api: API, options: Options) => {
     root
       .find(j.JSXOpeningElement, {
         name: { name: getLocalName(listCellImport) },
+      })
+      .forEach((comp) => {
+        const textProps = (
+          comp.value.attributes?.find(
+            (v) => v.type === 'JSXAttribute' && v.name.name === 'textProps',
+          ) as JSXAttribute | undefined
+        )?.value as JSXExpressionContainer;
+
+        deepConvertPropertyValue(textProps, 'color', convertColorPalette());
+      });
+  }
+
+  const accordionSummaryImport = findImportDeclaration(
+    'AccordionSummary',
+    '@wanteddev/wds',
+    j,
+    root,
+  );
+
+  if (accordionSummaryImport) {
+    root
+      .find(j.JSXOpeningElement, {
+        name: { name: accordionSummaryImport.imported.name },
       })
       .forEach((comp) => {
         const textProps = (
