5eadfa8a88097d789fdfa52f52725b19770b2f4e
Sh031224 <1cktmdgh2@gmail.com>
2025-04-15 13:52:36 +0900

    feat(wds): typography rem 단위로 변경

--- packages/wds/src/components/typography/style.ts
+++ packages/wds/src/components/typography/style.ts
@@ -8,93 +8,93 @@ export const variantMap: {
   [key in TypographyVariant]: SerializedStyles;
 } = {
   display1: css`
-    font-size: 56px;
-    line-height: 72px;
+    font-size: 3.5rem;
+    line-height: 4.5rem;
     letter-spacing: -0.0319em;
   `,
   display2: css`
-    font-size: 40px;
-    line-height: 52px;
+    font-size: 2.5rem;
+    line-height: 3.25rem;
     letter-spacing: -0.0282em;
   `,
   title1: css`
-    font-size: 36px;
-    line-height: 48px;
+    font-size: 2.25rem;
+    line-height: 3rem;
     letter-spacing: -0.027em;
   `,
   title2: css`
-    font-size: 28px;
-    line-height: 38px;
+    font-size: 1.7rem;
+    line-height: 2.375rem;
     letter-spacing: -0.0236em;
   `,
   title3: css`
-    font-size: 24px;
-    line-height: 32px;
+    font-size: 1.5rem;
+    line-height: 2rem;
     letter-spacing: -0.023em;
   `,
   heading1: css`
-    font-size: 22px;
-    line-height: 30px;
+    font-size: 1.375rem;
+    line-height: 1.875rem;
     letter-spacing: -0.0194em;
   `,
   heading2: css`
-    font-size: 20px;
-    line-height: 28px;
+    font-size: 1.25rem;
+    line-height: 1.75rem;
     letter-spacing: -0.012em;
   `,
   headline1: css`
-    font-size: 18px;
-    line-height: 26px;
+    font-size: 1.125rem;
+    line-height: 1.625rem;
     letter-spacing: -0.002em;
   `,
   headline2: css`
-    font-size: 17px;
-    line-height: 24px;
+    font-size: 1.063rem;
+    line-height: 1.5rem;
     letter-spacing: 0em;
   `,
   body1: css`
-    font-size: 16px;
-    line-height: 24px;
+    font-size: 1rem;
+    line-height: 1.5rem;
     letter-spacing: 0.0057em;
   `,
   'body1-reading': css`
-    font-size: 16px;
-    line-height: 26px;
+    font-size: 1rem;
+    line-height: 1.625rem;
     letter-spacing: 0.0057em;
   `,
   body2: css`
-    font-size: 15px;
-    line-height: 22px;
+    font-size: 0.938rem;
+    line-height: 1.375rem;
     letter-spacing: 0.0096em;
   `,
   'body2-reading': css`
-    font-size: 15px;
-    line-height: 24px;
+    font-size: 0.938rem;
+    line-height: 1.5rem;
     letter-spacing: 0.0096em;
   `,
   label1: css`
-    font-size: 14px;
-    line-height: 20px;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
     letter-spacing: 0.0145em;
   `,
   'label1-reading': css`
-    font-size: 14px;
-    line-height: 22px;
+    font-size: 0.875rem;
+    line-height: 1.375rem;
     letter-spacing: 0.0145em;
   `,
   label2: css`
-    font-size: 13px;
-    line-height: 18px;
+    font-size: 0.813rem;
+    line-height: 1.125rem;
     letter-spacing: 0.0194em;
   `,
   caption1: css`
-    font-size: 12px;
-    line-height: 16px;
+    font-size: 0.75rem;
+    line-height: 1rem;
     letter-spacing: 0.0252em;
   `,
   caption2: css`
-    font-size: 11px;
-    line-height: 14px;
+    font-size: 0.688rem;
+    line-height: 0.875rem;
     letter-spacing: 0.0311em;
   `,
 } as const;
