5934c9a9e8a8524020008dda289b21a9d6af14ab
Sh031224 <1cktmdgh2@gmail.com>
2025-04-22 15:03:49 +0900

    chore(ci): workflow call 추가

--- scripts/figma-icon-sync.mjs
+++ scripts/figma-icon-sync.mjs
@@ -1,17 +1,28 @@
 import fs from 'fs';
+import { execSync } from 'child_process';
 
-import * as changeCase from 'change-case';
-import shelljs from 'shelljs';
+const platform = process.env.PLATFORM || 'web';
+
+const WEB_IGNORE_ICONS = ['IconLogoInstagramColor', 'IconSymbol'];
+
+const ignoreIcons =
+  platform === 'web'
+    ? WEB_IGNORE_ICONS
+    : (process.env.IGNORE_ICONS || '').split(',');
+
+const kebabCase = (name) =>
+  name
+    .replace(/([a-z0-9])([A-Z])/g, '$1-$2')
+    .replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+    .toLowerCase();
+
+const exec = (cmd) => execSync(cmd).toString();
 
 const TOKEN = process.env.FIGMA_TOKEN;
-const FILE_KEY = '7RHtWV3Pw6I98UEDjbx5V1';
 const URL_BASE = 'https://api.figma.com/v1/files';
 const URL_BASE_IMAGES = 'https://api.figma.com/v1/images';
 
-const IGNORE_ICONS = ['IconLogoInstagramColor'];
-
-const ICON_NULL_COMPONENT = '501-7411';
-const ICON_RESPONSIVE_COMPONENT = '448-8266';
+const FILE_KEY = '7RHtWV3Pw6I98UEDjbx5V1';
 
 // Icon/Assets/Normal
 const ROOT_TRAVERSE_IDS = [
@@ -140,13 +151,7 @@ const fileRESTResponseToIconComponentsJSON = async (response) => {
                   .join('') +
                 type;
 
-              if (!IGNORE_ICONS.includes(name)) {
-                idsToNameAndComponentSetId[child.id] = [
-                  name,
-                  component.id,
-                  child.id,
-                ];
-              }
+              idsToNameAndComponentSetId[child.id] = [name, component.id];
             });
           }
         }
@@ -164,12 +169,8 @@ const fileRESTResponseToIconComponentsJSON = async (response) => {
   const processNodeId = async (nodeId) => {
     const fileResponse = await fetch(images[nodeId], { method: 'GET' });
     const svg = await fileResponse.text();
-    const [name, componentSetId, childId] = idsToNameAndComponentSetId[nodeId];
-    const figmaString = [];
-    res.imports.push(name);
-    res.exports.push(
-      `export { default as ${name} } from "./${changeCase.kebabCase(name)}";`,
-    );
+    const [name, componentSetId] = idsToNameAndComponentSetId[nodeId];
+
     const parsedName = name
       .replace('Icon', '')
       .replace('Color', '')
@@ -180,54 +181,23 @@ const fileRESTResponseToIconComponentsJSON = async (response) => {
           : a.charAt(0).toUpperCase() + a.substring(1),
       )
       .join('');
-    // figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${ICON_NULL_COMPONENT}", { variant: { Name: '${parsedName}' }, example: () => <${name} /> });
-    // figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${ICON_RESPONSIVE_COMPONENT}", { variant: { Name: '${parsedName}' }, props: { size: figma.enum('Size', { Small: '20px', Tiny: '16px', Normal: '24px', Medium: '28px', Large: '32px', }) }, example: ({ size }) => <${name} sx={{ fontSize: size }} /> });`,
-    figmaString.push(
-      `figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${componentSetId.replace(':', '-')}", { variant: { Name: '${parsedName}' }, example: () => <${name} /> });`,
-    );
-    res.files.push([
-      `${changeCase.kebabCase(name)}.tsx`,
-      `import { Box } from '@wanteddev/wds-engine';
-  import { forwardRef } from 'react';
-  
-  import type { SxProp } from '@wanteddev/wds-engine';
-  import type { ComponentPropsWithoutRef } from 'react';
-  
-  type Props = ComponentPropsWithoutRef<'svg'> & {
-    sx?: SxProp;
-  };
-  
-  const ${name} = forwardRef<SVGSVGElement, Props>((props, ref) => {
-    return (
-      ${svg
-        .replace(/width="(.*?)"/, '')
-        .replace(/height="(.*?)"/, '')
-        .replace(
-          'xmlns="http://www.w3.org/2000/svg"',
-          'xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" ref={ref} {...props}',
-        )
-        .replace('xmlns:xlink="http://www.w3.org/1999/xlink"', '')
-        .replace(svg.includes('viewBox="0 0 12 24"') ? 'width="1em"' : '', '')
-        .replaceAll('fill="#171719"', 'fill="currentColor"')
-        .replace(
-          /(stroke|fill|line|clip)-(.)/g,
-          (_, p1, p2) => p1 + p2.toUpperCase(),
-        )
-        .replace('<svg', '<Box as="svg"')
-        .replace('</svg', '</Box')}
-    )
-  });
-  
-  export default ${name};`,
-      figmaString.join('\n'),
-    ]);
+
+    res.push({
+      name,
+      content: svg,
+      id: componentSetId.replace(':', '-'),
+      parsedName,
+    });
   };
 
   console.log('Sleeping for ten seconds to wait for images to exist...');
   await new Promise((resolve) => setTimeout(resolve, 10000));
   console.log('Proceeding....');
 
-  const res = { files: [], exports: [], imports: [] };
+  /**
+   * @type {Array<{name: string, content: string, id: string, parsedName: string}>}
+   */
+  const res = [];
   const fails = [];
   await Promise.all(
     nodeIds.map(async (nodeId) => {
@@ -260,47 +230,127 @@ const fileRESTResponseToIconComponentsJSON = async (response) => {
 };
 
 const main = async () => {
-  const data = await getIconComponents();
-  fs.writeFileSync(
-    './Icons.figma.txt',
-    `import figma from "@figma/code-connect";\nimport {${data.imports.sort().join(', ')}} from "@wanteddev/wds-icon";`,
-  );
-  fs.writeFileSync(
-    './icons-index.txt',
-    [...data.exports, 'export { default as IconSymbol } from "./icon-symbol";']
-      .sort()
-      .join('\n'),
-  );
-  fs.writeFileSync('./icons.json', JSON.stringify(data.files, null, 2));
-
-  const json = JSON.parse(fs.readFileSync('./icons.json'));
-  fs.copyFileSync('./icons-index.txt', './packages/wds-icon/src/index.ts');
-  const figmaStarter = fs.readFileSync('./Icons.figma.txt');
-  fs.writeFileSync(
-    './figma/icons/index.figma.tsx',
-    `${figmaStarter}\nfigma.connect("<FIGMA_ICONS_BASE>?node-id=${ICON_NULL_COMPONENT}", { props: { children: figma.instance('Icon') }, example: (props) => <>{props.children}</> });figma.connect("<FIGMA_ICONS_BASE>?node-id=${ICON_RESPONSIVE_COMPONENT}", { props: { children: figma.instance('Icon') }, example: (props) => <>{props.children}</> });\n${json.map((a) => a[2]).join('\n')}`,
-  );
-  await Promise.all(
-    json.map(
-      ([fileName, fileContents]) =>
-        new Promise((resolve, reject) => {
-          fs.writeFile(
-            `./packages/wds-icon/src/${fileName}`,
-            fileContents,
-            (err) => (err ? reject(err) : resolve()),
-          );
-        }),
-    ),
+  const data = (await getIconComponents()).filter(
+    ({ name }) => !ignoreIcons.includes(name),
   );
 
-  fs.rmSync('./Icons.figma.txt');
-  fs.rmSync('./icons-index.txt');
-  fs.rmSync('./icons.json');
+  switch (platform) {
+    case 'web': {
+      /**
+       * @type {Array<[string, string]>}
+       */
+      const files = [];
 
-  shelljs.exec(
-    'pnpm jscodeshift ./packages/wds-icon/src --extensions=tsx, --parasr=tsx --transform=./packages/wds-codemod/src/transforms/svg-use-id.ts',
-  );
-  shelljs.exec('pnpm -F wds-icon lint:fix src');
+      /**
+       * @type {Array<string>}
+       */
+      const indexContents = [];
+
+      /**
+       * @type {Array<string>}
+       */
+      const figmaConnectContents = [];
+
+      data.forEach((icon) => {
+        const { name, content, id, parsedName } = icon;
+        const fileName = kebabCase(name);
+        const fileContent = `import { Box } from '@wanteddev/wds-engine';
+  import { forwardRef } from 'react';
+  
+  import type { SxProp } from '@wanteddev/wds-engine';
+  import type { ComponentPropsWithoutRef } from 'react';
+  
+  type Props = ComponentPropsWithoutRef<'svg'> & {
+    sx?: SxProp;
+  };
+  
+  const ${name} = forwardRef<SVGSVGElement, Props>((props, ref) => {
+    return (
+      ${content
+        .replace(/width="(.*?)"/, '')
+        .replace(/height="(.*?)"/, '')
+        .replace(
+          'xmlns="http://www.w3.org/2000/svg"',
+          'xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" ref={ref} {...props}',
+        )
+        .replace('xmlns:xlink="http://www.w3.org/1999/xlink"', '')
+        .replace(
+          content.includes('viewBox="0 0 12 24"') ? 'width="1em"' : '',
+          '',
+        )
+        .replaceAll('fill="#171719"', 'fill="currentColor"')
+        .replace(
+          /(stroke|fill|line|clip)-(.)/g,
+          (_, p1, p2) => p1 + p2.toUpperCase(),
+        )
+        .replace('<svg', '<Box as="svg"')
+        .replace('</svg', '</Box')}
+    )
+  });
+  
+  export default ${name};`;
+
+        files.push([fileName, fileContent]);
+
+        indexContents.push(
+          `export { default as ${name} } from "./${kebabCase(name)}";`,
+        );
+
+        // figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${ICON_NULL_COMPONENT}", { variant: { Name: '${parsedName}' }, example: () => <${name} /> });
+        // figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${ICON_RESPONSIVE_COMPONENT}", { variant: { Name: '${parsedName}' }, props: { size: figma.enum('Size', { Small: '20px', Tiny: '16px', Normal: '24px', Medium: '28px', Large: '32px', }) }, example: ({ size }) => <${name} sx={{ fontSize: size }} /> });`,
+        figmaConnectContents.push(
+          `figma.connect(${name}, "<FIGMA_ICONS_BASE>?node-id=${id}", { variant: { Name: '${parsedName}' }, example: () => <${name} /> });`,
+        );
+      });
+
+      fs.writeFileSync(
+        './figma/icons/index.figma.tsx',
+        `import figma from "@figma/code-connect";\nimport {${files.map(([name]) => name).join(', ')}} from "@wanteddev/wds-icon";
+${figmaConnectContents.join('\n')}`,
+      );
+
+      await Promise.all(
+        files.map(
+          ([fileName, fileContents]) =>
+            new Promise((resolve, reject) => {
+              fs.writeFile(
+                `./packages/wds-icon/src/${fileName}.tsx`,
+                fileContents,
+                (err) => (err ? reject(err) : resolve()),
+              );
+            }),
+        ),
+      );
+
+      fs.writeFileSync(
+        './packages/wds-icon/src/index.ts',
+        [
+          ...data.map(
+            ({ name }) =>
+              `export { default as ${name} } from "./${kebabCase(name)}";`,
+          ),
+          ...ignoreIcons.map(
+            (icon) =>
+              `export { default as ${icon} } from "./${kebabCase(icon)}";`,
+          ),
+        ]
+          .sort()
+          .join('\n'),
+        'utf-8',
+      );
+
+      exec(
+        'pnpm jscodeshift ./packages/wds-icon/src --extensions=tsx, --parasr=tsx --transform=./packages/wds-codemod/src/transforms/svg-use-id.ts',
+      );
+      exec('pnpm -F wds-icon lint:fix src');
+
+      break;
+    }
+    case 'android':
+      break;
+    case 'ios':
+      break;
+  }
 
   console.log('DONE!');
 };
