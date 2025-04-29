14b945e77434378a3b9ee5aed0e1ec1c79fe5f3c
Sh031224 <1cktmdgh2@gmail.com>
2025-04-29 15:59:30 +0900

    feat(wds): modal 높이가 작은 디바이스에서 유연하게 사이즈를 대응합니다

--- packages/wds/src/components/modal/style.ts
+++ packages/wds/src/components/modal/style.ts
@@ -391,7 +391,7 @@ const modalContainerVariant = (
       return css`
         border-radius: var(--wds-modal-popup-border-radius, 12px);
         animation: none;
-        max-height: 760px;
+        max-height: min(760px, 100%);
         padding: initial;
         overflow: hidden;
         transition: none;
