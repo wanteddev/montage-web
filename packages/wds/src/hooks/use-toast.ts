2c7587f679511274465bfa42d84d176dd740ee26
Sh031224 <1cktmdgh2@gmail.com>
2025-04-30 15:31:00 +0900

    feat(wds): toast, snackbar에 semandic duration 추가

--- packages/wds/src/hooks/use-toast.ts
+++ packages/wds/src/hooks/use-toast.ts
@@ -1,16 +1,40 @@
 import { useRegionStore } from '../stores/region-store';
 
-import type { RegionToastItem } from '../stores/region-store';
+import type {
+  RegionToastItem,
+  UseRegionStoreAddDuration,
+  WithUseRegionStoreAddDuration,
+} from '../stores/region-store';
 
 const useToast = () => {
   const storeAdd = useRegionStore((state) => state.add);
 
-  const add = (item: Omit<RegionToastItem, 'type'>) =>
+  const add = (
+    item: WithUseRegionStoreAddDuration<
+      Omit<RegionToastItem, 'type' | 'duration'>
+    >,
+  ) => {
+    const getDuration = (duration?: UseRegionStoreAddDuration): number => {
+      if (typeof duration === 'number') {
+        return duration;
+      }
+
+      switch (duration) {
+        case 'short':
+          return 3000;
+        case 'long':
+          return 5000;
+        default:
+          return getDuration('short');
+      }
+    };
+
     storeAdd({
       type: 'toast',
       ...item,
-      duration: item.duration ?? 3000,
+      duration: getDuration(item.duration),
     });
+  };
 
   return add;
 };
