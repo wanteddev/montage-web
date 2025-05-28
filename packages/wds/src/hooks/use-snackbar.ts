314338e43b2dfe12164448a8f7ada059d16f7f22
Sh031224 <1cktmdgh2@gmail.com>
2025-05-28 18:38:44 +0900

    feat(wds): region store 정리

--- packages/wds/src/hooks/use-snackbar.ts
+++ packages/wds/src/hooks/use-snackbar.ts
@@ -1,40 +1,15 @@
 import { useRegionStore } from '../stores/region-store';
 
-import type {
-  RegionSnackbarItem,
-  UseRegionStoreAddDuration,
-  WithUseRegionStoreAddDuration,
-} from '../stores/region-store';
+import type { RegionSnackbarItem } from '../stores/region-store';
 
 const useSnackbar = () => {
   const storeAdd = useRegionStore((state) => state.add);
 
-  const add = (
-    item: WithUseRegionStoreAddDuration<
-      Omit<RegionSnackbarItem, 'type' | 'duration'>
-    >,
-  ) => {
-    const getDuration = (duration?: UseRegionStoreAddDuration): number => {
-      if (typeof duration === 'number') {
-        return duration;
-      }
-
-      switch (duration) {
-        case 'short':
-          return 4000;
-        case 'long':
-          return 16000;
-        default:
-          return getDuration('short');
-      }
-    };
-
+  const add = (item: Omit<RegionSnackbarItem, 'type'>) =>
     storeAdd({
       type: 'snackbar',
       ...item,
-      duration: getDuration(item.duration),
     });
-  };
 
   return add;
 };
--- packages/wds/src/hooks/use-toast.ts
+++ packages/wds/src/hooks/use-toast.ts
@@ -1,40 +1,15 @@
 import { useRegionStore } from '../stores/region-store';
 
-import type {
-  RegionToastItem,
-  UseRegionStoreAddDuration,
-  WithUseRegionStoreAddDuration,
-} from '../stores/region-store';
+import type { RegionToastItem } from '../stores/region-store';
 
 const useToast = () => {
   const storeAdd = useRegionStore((state) => state.add);
 
-  const add = (
-    item: WithUseRegionStoreAddDuration<
-      Omit<RegionToastItem, 'type' | 'duration'>
-    >,
-  ) => {
-    const getDuration = (duration?: UseRegionStoreAddDuration): number => {
-      if (typeof duration === 'number') {
-        return duration;
-      }
-
-      switch (duration) {
-        case 'short':
-          return 3000;
-        case 'long':
-          return 5000;
-        default:
-          return getDuration('short');
-      }
-    };
-
+  const add = (item: Omit<RegionToastItem, 'type'>) =>
     storeAdd({
       type: 'toast',
       ...item,
-      duration: getDuration(item.duration),
     });
-  };
 
   return add;
 };
