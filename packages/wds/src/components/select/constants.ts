a644041737e3d3725943ecb12e2f3560bad2c1bd
차승호 <1cktmdgh2@gmail.com>
2025-07-15 14:46:41 +0900

    feat(wds): provide ListCellContent as a subcomponent instead of ListCellContent (#362)

--- packages/wds/src/components/select/constants.ts
+++ packages/wds/src/components/select/constants.ts
@@ -1,4 +1,5 @@
 export const SELECT_NAME = 'Select';
+export const SELECT_CONTENT_NAME = 'SelectContent';
 export const OPTION_GROUP_NAME = 'OptionGroup';
 export const OPTION_NAME = 'Option';
-export const SELECT_CONTENT_NAME = 'SelectContent';
+export const OPTION_CONTENT_NAME = 'OptionContent';
