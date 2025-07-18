18707f6ff891016e44cfdbb0a2539750ade132c1
차승호 <1cktmdgh2@gmail.com>
2025-07-18 17:52:39 +0900

    feat(wds): improve accessibility of menu-type components (#371)

--- packages/wds/src/components/select/context.ts
+++ packages/wds/src/components/select/context.ts
@@ -1,11 +1,13 @@
-import createLooseContext from '../../hooks/use-loose-context';
+import { createContext } from '@radix-ui/react-context';
 
 import { SELECT_NAME } from './constants';
 
 type SelectContextType = {
   onOpenChange: (open: boolean) => void;
   enableMenuActionArea?: boolean;
+  value?: string | Array<string>;
+  isMultiple?: boolean;
 };
 
 export const [SelectProvider, useSelectContext] =
-  createLooseContext<SelectContextType>(SELECT_NAME);
+  createContext<SelectContextType>(SELECT_NAME);
