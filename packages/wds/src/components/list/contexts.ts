d1c798179338d266c9ca831984a3819fc747c47d
차승호 <1cktmdgh2@gmail.com>
2025-07-18 17:52:39 +0900

    feat(wds): improve accessibility of menu-type components (#371)

--- packages/wds/src/components/list/contexts.ts
+++ packages/wds/src/components/list/contexts.ts
@@ -6,7 +6,10 @@ import type { ListCellProps } from './types';
 
 type ListCellContextType = Required<
   Pick<ListCellProps, 'active' | 'disabled' | 'ellipsis' | 'alignItems'>
->;
+> & {
+  textId: string;
+  captionId: string;
+};
 
 export const [ListCellProvider, useListCellContext] =
   createContext<ListCellContextType>(LIST_CELL_NAME);
