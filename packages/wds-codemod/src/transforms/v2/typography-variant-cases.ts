1a1a734ce6e925177138a149585be55500216858
Sh031224 <1cktmdgh2@gmail.com>
2025-07-18 18:16:56 +0900

    feat(wds): catalog for dependecies and cleanup codemod

--- packages/wds-codemod/src/transforms/v2/typography-variant-cases.ts
+++ packages/wds-codemod/src/transforms/v2/typography-variant-cases.ts
@@ -1,4 +1,4 @@
-import { deepConvertPropertyValue, findImportDeclaration } from '../helpers';
+import { deepConvertPropertyValue, findImportDeclaration } from '../../helpers';
 
 import type {
   API,
