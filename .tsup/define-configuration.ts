b52f899432885a7b50acc884cd68dea12a8e5eb2
차승호 <1cktmdgh2@gmail.com>
2025-07-21 10:11:20 +0900

    test(*): setup vitest and adding accessibility tests (#375)

--- .tsup/define-configuration.ts
+++ .tsup/define-configuration.ts
@@ -11,13 +11,14 @@ export const defineConfiguration = (opts: Partial<Options>): Options => {
     treeshake: true,
     bundle: true,
     silent: true,
-    external: [
-      'react',
-      'react-dom',
-      'next',
-      'react-hook-form',
-    ],
+    external: ['react', 'react-dom', 'next', 'react-hook-form'],
     esbuildPlugins: [esbuildPluginFilePathExtensions({ cjsExtension: 'js' })],
     ...opts,
+    entry: Array.isArray(opts.entry)
+      ? opts.entry.concat(['!src/**/*.test.*'])
+      : {
+          ...opts.entry,
+          test: '!src/**/*.test.*',
+        },
   };
 };
