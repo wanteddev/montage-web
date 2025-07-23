7d7a8e1eaf3011a1cd2eb53c105c1e723ae17e71
Sh031224 <1cktmdgh2@gmail.com>
2025-07-23 14:26:08 +0900

    test(wds, wds-lottie): improve accessibility and write test code for components (#378)

--- .vitest/setup-tests.ts
+++ .vitest/setup-tests.ts
@@ -1,5 +1,31 @@
 import '@testing-library/jest-dom/vitest';
 import * as axeMatchers from 'vitest-axe/matchers';
-import { expect } from 'vitest';
+import { expect, vi } from 'vitest';
 
 expect.extend(axeMatchers);
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+global.ResizeObserver = class ResizeObserver {
+  cb: any;
+  constructor(cb: any) {
+    this.cb = cb;
+  }
+  observe() {
+    this.cb([{ borderBoxSize: { inlineSize: 0, blockSize: 0 } }]);
+  }
+  unobserve() {}
+  disconnect() {}
+};
