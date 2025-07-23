a9ccc3b591a2b7235739a2b15c5805fca6d9ff67
Sh031224 <1cktmdgh2@gmail.com>
2025-07-23 14:26:08 +0900

    test(wds, wds-lottie): improve accessibility and write test code for components (#378)

---
+++ packages/wds/src/components/snackbar/index.test.tsx
@@ -0,0 +1,99 @@
+import {
+  act,
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
+import { axe } from 'vitest-axe';
+
+import {
+  Snackbar,
+  SnackbarAction,
+  SnackbarContent,
+  SnackbarDescription,
+  SnackbarHeading,
+} from '.';
+
+const DEFAULT_DURATION = 4000;
+
+describe('when given snackbar component', () => {
+  beforeEach(() => {
+    vi.mock('../animation-presence', () => ({
+      AnimationPresence: ({
+        children,
+        present,
+      }: {
+        children: React.ReactNode;
+        present: boolean;
+      }) => (present ? children : null),
+    }));
+
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+
+    render(
+      <Snackbar defaultOpen data-testid="snackbar">
+        <SnackbarContent data-testid="snackbar-content">
+          <SnackbarHeading data-testid="snackbar-heading">
+            Snackbar Heading
+          </SnackbarHeading>
+          <SnackbarDescription data-testid="snackbar-description">
+            Snackbar Description
+          </SnackbarDescription>
+          <SnackbarAction data-testid="snackbar-action">
+            Snackbar Action
+          </SnackbarAction>
+        </SnackbarContent>
+      </Snackbar>,
+    );
+  });
+
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.useRealTimers();
+    cleanup();
+  });
+
+  it(`should render toast component and ${DEFAULT_DURATION}ms after close`, async () => {
+    expect(screen.getByTestId('snackbar')).toBeInTheDocument();
+
+    await act(() => vi.advanceTimersByTime(DEFAULT_DURATION));
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('snackbar')).not.toBeInTheDocument();
+    });
+  });
+
+  it('should not close snackbar if mouse is over the toast container after open', async () => {
+    expect(screen.getByTestId('snackbar')).toBeInTheDocument();
+
+    await act(() => vi.advanceTimersByTime(DEFAULT_DURATION / 2));
+
+    fireEvent.mouseEnter(screen.getByTestId('snackbar'));
+
+    await act(() => vi.advanceTimersByTime(DEFAULT_DURATION));
+
+    await waitFor(() => {
+      expect(screen.getByTestId('snackbar')).toBeInTheDocument();
+    });
+
+    fireEvent.mouseLeave(screen.getByTestId('snackbar'));
+
+    await act(() => vi.advanceTimersByTime(DEFAULT_DURATION / 2));
+
+    await waitFor(() => {
+      expect(screen.queryByTestId('snackbar')).not.toBeInTheDocument();
+    });
+  });
+
+  it('should pass accessibility test', async () => {
+    expect(await axe(screen.getByTestId('snackbar'))).toHaveNoViolations();
+    expect(
+      await axe(screen.getByTestId('snackbar-content')),
+    ).toHaveNoViolations();
+    expect(
+      await axe(screen.getByTestId('snackbar-heading')),
+    ).toHaveNoViolations();
+  });
+});
---
+++ packages/wds/src/components/tooltip/index.test.tsx
@@ -0,0 +1,256 @@
+import {
+  act,
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react';
+import { axe } from 'vitest-axe';
+
+import { Button } from '../button';
+
+import { Tooltip, TooltipContent, TooltipGroup, TooltipTrigger } from '.';
+
+describe('when given hover mode tooltip component', () => {
+  beforeEach(() => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+
+    vi.mock('../animation-presence', () => ({
+      AnimationPresence: ({
+        children,
+        present,
+      }: {
+        children: React.ReactNode;
+        present: boolean;
+      }) => (present ? children : null),
+    }));
+
+    render(
+      <Tooltip enterDelay={200} leaveDelay={200}>
+        <TooltipTrigger>
+          <Button data-testid="tooltip-trigger">Tooltip Trigger</Button>
+        </TooltipTrigger>
+        <TooltipContent data-testid="tooltip-content">
+          Tooltip Content
+        </TooltipContent>
+      </Tooltip>,
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+    vi.clearAllMocks();
+    vi.useRealTimers();
+  });
+
+  it('should render tooltip content with mouse enter', () => {
+    expect(screen.getByTestId('tooltip-trigger')).toBeInTheDocument();
+    expect(screen.queryByTestId('tooltip-content')).not.toBeInTheDocument();
+
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger'));
+
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
+
+    fireEvent.mouseLeave(screen.getByTestId('tooltip-trigger'));
+
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.queryByTestId('tooltip-content')).not.toBeInTheDocument();
+  });
+
+  it('should render tooltip content with focus', () => {
+    expect(screen.getByTestId('tooltip-trigger')).toBeInTheDocument();
+    expect(screen.queryByTestId('tooltip-content')).not.toBeInTheDocument();
+
+    fireEvent.focus(screen.getByTestId('tooltip-trigger'));
+
+    act(() => {
+      vi.advanceTimersByTime(0);
+    });
+
+    expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
+
+    fireEvent.blur(screen.getByTestId('tooltip-trigger'));
+
+    act(() => {
+      vi.advanceTimersByTime(0);
+    });
+
+    expect(screen.queryByTestId('tooltip-content')).not.toBeInTheDocument();
+  });
+
+  it('should pass accessibility tests', async () => {
+    expect(
+      await axe(screen.getByTestId('tooltip-trigger')),
+    ).toHaveNoViolations();
+
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger'));
+
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
+
+    expect(
+      await axe(screen.getByTestId('tooltip-content')),
+    ).toHaveNoViolations();
+  });
+});
+
+describe('when given click mode tooltip component', () => {
+  beforeEach(() => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+
+    render(
+      <Tooltip mode="click">
+        <TooltipTrigger>
+          <Button data-testid="tooltip-trigger">Tooltip Trigger</Button>
+        </TooltipTrigger>
+        <TooltipContent data-testid="tooltip-content">
+          Tooltip Content
+        </TooltipContent>
+      </Tooltip>,
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+    vi.useRealTimers();
+  });
+
+  it('should render tooltip content with click', () => {
+    expect(screen.getByTestId('tooltip-trigger')).toBeInTheDocument();
+    expect(screen.queryByTestId('tooltip-content')).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByTestId('tooltip-trigger'));
+
+    act(() => {
+      vi.runAllTimers();
+    });
+
+    expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
+  });
+});
+
+describe('when given always mode tooltip component', () => {
+  beforeEach(() => {
+    render(
+      <Tooltip mode="always" open>
+        <TooltipTrigger>
+          <Button data-testid="tooltip-trigger">Tooltip Trigger</Button>
+        </TooltipTrigger>
+        <TooltipContent data-testid="tooltip-content">
+          Tooltip Content
+        </TooltipContent>
+      </Tooltip>,
+    );
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  it('should keep open state', () => {
+    expect(screen.getByTestId('tooltip-trigger')).toBeInTheDocument();
+    expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
+
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger'));
+    fireEvent.mouseLeave(screen.getByTestId('tooltip-trigger'));
+    fireEvent.click(screen.getByTestId('tooltip-trigger'));
+
+    expect(screen.getByTestId('tooltip-content')).toBeInTheDocument();
+  });
+});
+
+describe('when given tooltip with tooltip group component', () => {
+  beforeEach(() => {
+    vi.useFakeTimers({ shouldAdvanceTime: true });
+
+    render(
+      <TooltipGroup skipDelayDuration={350}>
+        <Tooltip enterDelay={200} leaveDelay={200}>
+          <TooltipTrigger>
+            <Button data-testid="tooltip-trigger-1">Trigger 1</Button>
+          </TooltipTrigger>
+          <TooltipContent data-testid="tooltip-content-1">
+            Tooltip Content 1
+          </TooltipContent>
+        </Tooltip>
+        <Tooltip enterDelay={200} leaveDelay={200}>
+          <TooltipTrigger>
+            <Button data-testid="tooltip-trigger-2">Trigger 2</Button>
+          </TooltipTrigger>
+          <TooltipContent data-testid="tooltip-content-2">
+            Tooltip Content 2
+          </TooltipContent>
+        </Tooltip>
+      </TooltipGroup>,
+    );
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+    cleanup();
+  });
+
+  it('should open the first tooltip after enter delay', () => {
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger-1'));
+
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.getByTestId('tooltip-content-1')).toBeInTheDocument();
+  });
+
+  it('should open the second tooltip immediately when moving from the first to the second within skipDelayDuration', () => {
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger-1'));
+
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.getByTestId('tooltip-content-1')).toBeInTheDocument();
+
+    fireEvent.mouseLeave(screen.getByTestId('tooltip-trigger-1'));
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger-2'));
+
+    act(() => {
+      vi.advanceTimersByTime(0);
+    });
+
+    expect(screen.getByTestId('tooltip-content-2')).toBeInTheDocument();
+  });
+
+  it('should apply enter delay again if moving to another tooltip after skipDelayDuration', () => {
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger-1'));
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.getByTestId('tooltip-content-1')).toBeInTheDocument();
+
+    fireEvent.mouseLeave(screen.getByTestId('tooltip-trigger-1'));
+
+    act(() => {
+      vi.advanceTimersByTime(350);
+    });
+
+    fireEvent.mouseEnter(screen.getByTestId('tooltip-trigger-2'));
+
+    expect(screen.queryByTestId('tooltip-content-2')).not.toBeInTheDocument();
+
+    act(() => {
+      vi.advanceTimersByTime(200);
+    });
+
+    expect(screen.getByTestId('tooltip-content-2')).toBeInTheDocument();
+  });
+});
