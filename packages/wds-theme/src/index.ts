d22ed2a385ecdc18ec36aeb1bef924d48d6fda26
차승호 <1cktmdgh2@gmail.com>
2025-07-23 18:45:00 +0900

    refactor(*): improve accessibility and testing, and clean up code (#382)

--- packages/wds-theme/src/index.ts
+++ packages/wds-theme/src/index.ts
@@ -1,37 +1,3 @@
-import type { darkTheme, lightTheme } from './theme';
-
 export * from './theme';
 export * from './utils';
-
-export type Theme = typeof lightTheme | typeof darkTheme;
-
-export type BreakPoint = Theme['breakpoint'];
-export type Spacing = Theme['spacing'];
-
-type MergeWithDot<T extends string> = T extends '' ? '' : `.${T}`;
-
-type ObjectToNestedKeys<T> = (
-  T extends object
-    ? {
-        [K in Exclude<
-          keyof T,
-          symbol
-        >]: `${K}${MergeWithDot<ObjectToNestedKeys<T[K]>>}`;
-      }[Exclude<keyof T, symbol>]
-    : ''
-) extends infer D
-  ? Extract<D, string>
-  : never;
-
-export type ThemeToken = ObjectToNestedKeys<Theme>;
-export type ThemeColorsToken =
-  | ObjectToNestedKeys<Pick<Theme, 'atomic'>>
-  | Exclude<
-      ObjectToNestedKeys<Pick<Theme, 'semantic'>>,
-      | 'semantic.platform.ios.navigation'
-      | 'semantic.elevation.shadow.emphasize'
-      | 'semantic.elevation.shadow.normal'
-      | 'semantic.elevation.shadow.heavy'
-      | 'semantic.elevation.shadow.strong'
-    >;
-export type ThemeOpacityToken = ObjectToNestedKeys<Pick<Theme, 'opacity'>>;
+export * from './types';
