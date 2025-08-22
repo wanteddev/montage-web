f17904476064eb9506178392cc8b72103d95d46a
차승호 <1cktmdgh2@gmail.com>
2025-08-22 16:40:05 +0900

    feat(eslint-plugin-wds): create eslint plugin for wds package (#405)

--- packages/eslint-plugin-wds/.eslintrc.cjs
+++ packages/eslint-plugin-wds/.eslintrc.cjs
@@ -3,7 +3,10 @@ const path = require('path');
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: [path.join(__dirname, '../../.eslintrc.cjs')],
+  extends: [
+    path.join(__dirname, '../../.eslintrc.cjs'),
+    'plugin:eslint-plugin/recommended',
+  ],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
@@ -13,16 +16,16 @@ module.exports = {
       typescript: {
         project: [
           __dirname + '/tsconfig.json',
-          __dirname + '/tsconfig.node.json',
+          __dirname + '/tsconfig.test.json',
         ],
       },
     },
   },
   overrides: [
     {
-      files: ['tsup.config.ts'],
+      files: ['tests/**.ts', 'src/**/*.test.ts', 'tsup.config.ts'],
       parserOptions: {
-        project: ['./tsconfig.node.json'],
+        project: ['./tsconfig.test.json'],
       },
     },
   ],
