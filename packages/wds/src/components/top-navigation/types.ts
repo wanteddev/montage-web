cfceb62b81b6de35eff91c224839ad310ebe658a
차승호 <1cktmdgh2@gmail.com>
2025-09-29 11:10:42 +0900

    feat(wds): add search, display variant in top navigation and updated design (#447)

--- packages/wds/src/components/top-navigation/types.ts
+++ packages/wds/src/components/top-navigation/types.ts
@@ -8,11 +8,11 @@ import type { ReactNode } from 'react';
 export type TopNavigationProps = WithSxProps<
   Merge<
     {
-      variant?: 'normal' | 'floating' | 'extended';
+      variant?: 'normal' | 'floating' | 'display' | 'search';
       trailingContent?: ReactNode;
       leadingContent?: ReactNode;
       toolbar?: ReactNode;
-      scrolled?: boolean;
+      background?: boolean;
       /**
        * The id to be assigned to the navigation title.
        */
@@ -25,14 +25,6 @@ export type TopNavigationProps = WithSxProps<
 
 export type TopNavigationButtonProps = WithSxProps<{
   variant?: 'text' | 'icon';
-  /**
-   * When using `floating` navigation, if `alternative` is true, the black theme is activated.
-   */
-  alternative?: boolean;
-  /**
-   * When using `floating` navigation, it determines whether to use the background icon.
-   */
-  background?: boolean;
   disabled?: boolean;
   size?: number | 'medium' | 'small';
   children?: ReactNode;
