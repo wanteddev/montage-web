0091392fa956d49b72ca1164696b283fafd06b38
Sh031224 <1cktmdgh2@gmail.com>
2025-11-11 15:56:00 +0900

    refactor(eslint-plugin-wds): apply code reveiw

--- packages/eslint-plugin-wds/tsdown.config.ts
+++ packages/eslint-plugin-wds/tsdown.config.ts
@@ -1,17 +1,5 @@
 import { defineConfiguration } from '../../.tsdown/define-configuration.ts';
-import { injectUseClient } from '../../.tsdown/inject-use-client.ts';
 
 export default defineConfiguration({
   entry: ['src/**/*.ts', 'src/**/*.tsx'],
-  onSuccess: () =>
-    injectUseClient([
-      './dist/components/*/*.{js,mjs}',
-      './dist/hooks/*.{js,mjs}',
-      './dist/stores/*.{js,mjs}',
-      './dist/theme-provider/*.{js,mjs}',
-      './dist/utils/layout.{js,mjs}',
-      './dist/utils/responsive-props.{js,mjs}',
-      './dist/utils/typography.{js,mjs}',
-      './dist/utils/children.{js,mjs}',
-    ]),
 });
