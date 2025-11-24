a69d29651cc250f01146a90532e3f185b1493d89
차승호 <1cktmdgh2@gmail.com>
2025-11-24 18:13:39 +0900

    chore(ci): add commonjs node resolution test (#482)

---
+++ tests/commonjs/src/index.cjs
@@ -0,0 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { Button, css, theme } = require('@wanteddev/wds');
+
+console.assert(Button, 'Button');
+console.assert(theme, 'theme');
+console.assert(css, 'css');
