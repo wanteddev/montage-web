bd25c9303925554cb6f4436ea401586a10145f5f
Sh031224 <1cktmdgh2@gmail.com>
2026-02-06 14:39:39 +0900

    fix(wds): next.js 16부터  시 타입 오류 나는 현상

--- packages/wds/src/components/toast/types.ts
+++ packages/wds/src/components/toast/types.ts
@@ -4,25 +4,27 @@ import type { TypographyProps } from '../typography/types';
 import type { FlexBoxProps } from '../flex-box/types';
 import type { RegionToastItem } from '../../stores/region-store';
 import type { ReactNode } from 'react';
+import type { WithSxProps } from '@wanteddev/wds-engine';
 
 export type ToastProps = Pick<
   RegionToastItem,
   'duration' | 'variant' | 'icon' | 'onAnimationEnd'
-> & {
-  /** Whether the toast is open by default. */
-  defaultOpen?: boolean;
-  /** Whether the toast is open. */
-  open?: boolean;
-  /** Callback function when the open state changes. */
-  onOpenChange?: (open: boolean) => void;
-  /** The container of the toast. */
-  container?: PortalOrFragmentProps['container'];
-  /** Whether to disable the portal. */
-  disablePortal?: PortalOrFragmentProps['disablePortal'];
-  /** Whether to disable the animation. */
-  disableAnimation?: boolean;
-  children?: ReactNode;
-};
+> &
+  WithSxProps<{
+    /** Whether the toast is open by default. */
+    defaultOpen?: boolean;
+    /** Whether the toast is open. */
+    open?: boolean;
+    /** Callback function when the open state changes. */
+    onOpenChange?: (open: boolean) => void;
+    /** The container of the toast. */
+    container?: PortalOrFragmentProps['container'];
+    /** Whether to disable the portal. */
+    disablePortal?: PortalOrFragmentProps['disablePortal'];
+    /** Whether to disable the animation. */
+    disableAnimation?: boolean;
+    children?: ReactNode;
+  }>;
 
 export type ToastContainerProps = FlexBoxProps;
 export type ToastIconProps = SlotProps;
