606eb95adb6b483d3ba0cf0301b0ed239137fe5c
Sh031224 <1cktmdgh2@gmail.com>
2026-02-09 08:55:05 +0900

    fix(docs): 빌드 오류 수정

--- docs/src/app/(with-lnb)/docs/[...slug]/page.tsx
+++ docs/src/app/(with-lnb)/docs/[...slug]/page.tsx
@@ -32,7 +32,7 @@ export const dynamic = 'force-static';
 
 export const generateStaticParams = async () => {
   const frontmatter = (await getAllFrontmatter()).filter(
-    (item) => !item.isPrivate,
+    (item) => !item.isExternal,
   );
 
   return frontmatter;
