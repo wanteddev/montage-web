7c5e15b63d1ff15b6b6bd8861c45d6ff4e5dc6e3
Sh031224 <1cktmdgh2@gmail.com>
2026-03-10 15:01:10 +0900

    chore: figma ui kit 업데이트 날짜 조정

--- docs/src/features/home/components/resources/constants.ts
+++ docs/src/features/home/components/resources/constants.ts
@@ -2,14 +2,14 @@ export const RESOURCE_ITEMS = [
   {
     title:
       'Wanted <span class="max-xl:hidden">Design System </span>UI KIT<span class="max-sm:hidden"> for Figma</span>',
-    updatedAt: '<span class="max-sm:hidden">Last Updated. </span>2025.06.04',
+    updatedAt: '<span class="max-sm:hidden">Last Updated. </span>2026.03.09',
     href: 'https://www.figma.com/community/file/1355516515676178246',
     lottie: '/home/resources/1.json',
   },
   {
     title:
       '<span class="max-xl:hidden">Native </span>Platform UI KIT<span class="max-sm:hidden"> for Figma</span>',
-    updatedAt: '<span class="max-sm:hidden">Last Updated. </span>2025.06.04',
+    updatedAt: '<span class="max-sm:hidden">Last Updated. </span>2026.03.09',
     href: 'https://www.figma.com/community/file/1509019654708670821',
     lottie: '/home/resources/2.json',
   },
