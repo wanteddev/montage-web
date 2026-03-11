4be1ec559057884673c43708ca04db73be6dca5c
차승호 <1cktmdgh2@gmail.com>
2026-03-11 15:19:49 +0900

    feat(wds): bottom sheet handle 활성화 된 경우 기본적으로 바로 닫히도록 변경 (#502)

--- packages/wds/src/components/modal/helpers.tsx
+++ packages/wds/src/components/modal/helpers.tsx
@@ -1,3 +1,5 @@
+import { BOTTOM_SHEET_SHADOW } from './constants';
+
 export const isTouchEvent = (
   value: MouseEvent | TouchEvent | React.MouseEvent | React.TouchEvent,
 ): value is TouchEvent | React.TouchEvent => value.type.includes('touch');
@@ -22,4 +24,37 @@ export const isMouseDownOnPeek = (
   const clientY = isTouchEvent(e) ? e.touches[0]!.clientY : e.clientY;
 
   return clientY >= top && clientY <= top + peekHeight;
+};
+
+export const applyPeekState = (
+  container: HTMLDivElement,
+  dimmer: HTMLDivElement | null,
+  peek: number,
+) => {
+  container.style.setProperty(
+    '--wds-modal-translate',
+    `calc(100% - ${peek}px)`,
+  );
+  container.style.setProperty('box-shadow', BOTTOM_SHEET_SHADOW);
+  dimmer?.style.setProperty('opacity', '0');
+};
+
+export const applyVisibleState = (
+  container: HTMLDivElement,
+  dimmer: HTMLDivElement | null,
+) => {
+  container.style.setProperty('--wds-modal-translate', '0px');
+  container.style.removeProperty('box-shadow');
+  dimmer?.style.setProperty('opacity', '1');
+};
+
+export const resetDragStyles = (
+  container: HTMLDivElement,
+  dimmer: HTMLDivElement | null,
+) => {
+  container.style.removeProperty('transition');
+  container.style.removeProperty('--wds-modal-translate');
+  container.style.removeProperty('box-shadow');
+  dimmer?.style.removeProperty('transition');
+  dimmer?.style.removeProperty('opacity');
 };
