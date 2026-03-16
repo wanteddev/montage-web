0807085eb949277ccb5d5e21a98f8d3e756e1740
Sh031224 <1cktmdgh2@gmail.com>
2026-03-16 10:43:00 +0900

    feat(codemod): 패키지명 변경 마이그레이션 추가 (#522)

--- packages/codemod/src/helpers/index.ts
+++ packages/codemod/src/helpers/index.ts
@@ -28,28 +28,51 @@ export const getImportedName = (importSpecifier: ImportSpecifier) => {
 
 export const findImportDeclaration = (
   name: string,
-  from: string,
+  from: Array<string> | string,
   j: JSCodeshift,
   root: Collection<any>,
 ) => {
   let result: ImportSpecifier | undefined;
 
-  root
-    .find(j.ImportDeclaration, {
-      source: { value: from },
-    })
-    .forEach((importDeclaration) => {
-      importDeclaration.node.specifiers?.forEach((specifier) => {
-        if (
-          specifier.type === 'ImportSpecifier' &&
-          specifier.imported.name === name
-        ) {
-          result = specifier;
+  if (Array.isArray(from)) {
+    from.forEach((f) => {
+      if (result) {
+        return;
+      }
 
-          return;
-        }
-      });
+      root
+        .find(j.ImportDeclaration, {
+          source: { value: f },
+        })
+        .forEach((importDeclaration) => {
+          importDeclaration.node.specifiers?.forEach((specifier) => {
+            if (
+              specifier.type === 'ImportSpecifier' &&
+              specifier.imported.name === name
+            ) {
+              result = specifier;
+              return;
+            }
+          });
+        });
     });
+  } else {
+    root
+      .find(j.ImportDeclaration, {
+        source: { value: from },
+      })
+      .forEach((importDeclaration) => {
+        importDeclaration.node.specifiers?.forEach((specifier) => {
+          if (
+            specifier.type === 'ImportSpecifier' &&
+            specifier.imported.name === name
+          ) {
+            result = specifier;
+            return;
+          }
+        });
+      });
+  }
 
   return result;
 };
